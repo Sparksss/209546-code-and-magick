fa18e63b995f9134d517bff77601010328baad32
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-12 22:18:42 +0300

    Создана прямоугольная область для вывода результатов и добавлены 2 надписи

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,8 @@
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'white';
-  ctx.fillRect(100, 10, 270, 420);
+  ctx.fillRect(100, 10, 420, 270);
+  ctx.fillStyle = 'black';
+  ctx.font = '16px serif';
+  ctx.fillText('Ура вы победили!', 110, 50);
+  ctx.fillText('Список результатов:', 110, 70);
 };
