aac96c56b3dd223fb984c501074f5aae1454e10b
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-13 22:03:27 +0300

    Сделанно: гистограмма(пока все колонки одинаковые), вывод худшего результата, вывод времени

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,21 @@
+'use strict';
+
 window.renderStatistics = function (ctx, names, times) {
+  var getMaxTime = function () {
+    var max = -1;
+    var maxIndex = -1;
+    var indexMaxTime = [];
+    for (var i = 0; i < times.length; i++) {
+      var time = times[i];
+      if (time > max) {
+        max = time;
+        maxIndex = i;
+      }
+    }
+    indexMaxTime.push(parseInt(max, 10));
+    indexMaxTime.push(maxIndex);
+    return indexMaxTime;
+  };
   ctx.fillStyle = 'white';
   ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
   ctx.shadowOffsetX = 10;
@@ -10,11 +27,38 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'black';
   ctx.font = '16px serif';
   ctx.fillText('Ура вы победили!', 110, 50);
-  ctx.fillText('Список результатов:', 110, 70);
+  ctx.fillText(' Худшее время: ' + getMaxTime(times)[0] + ' мс у игрока ' + names[getMaxTime(times)[1]], 110, 70);
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(110, 100, 40,150);
-  ctx.fillStyle = 'black';
-  ctx.font = '16px serif';
-  ctx.fillText(names[0], 110, 270);
+
+  var getRandomColor = function () {
+    var charsAndNumbers = '0123456789ABCDEF';
+    var color = '#0000';
+    for (var i = 0; i < 2; i++) {
+      color += charsAndNumbers[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  var positionX = 150;
+  var positionY = 100;
+  var widthSize = 40;
+  var heightSize = 150;
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillStyle = 'black';
+    ctx.font = '16px serif';
+    ctx.fillText(parseInt(times[i], 10), positionX, 90);
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = getRandomColor();
+    }
+    ctx.fillRect(positionX, positionY, widthSize, heightSize);
+    ctx.fillStyle = 'black';
+    ctx.font = '16px serif';
+    ctx.fillText(names[i], positionX, 270);
+    if (i !== names.length - 1) {
+      positionX += 90;
+    }
+  }
+
 };
