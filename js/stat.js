22d9fc2c05326ef6e779628bcd30f546910a15f8
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-15 00:08:40 +0300

    Рассортированно по функциям

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var renderCloud = function (ctx, colorCloud, x, y) {
+  ctx.fillStyle = colorCloud;
+  ctx.fillRect(x, y, 420, 270);
+};
 var getMaxTime = function (times) {
   var max = -1;
   var maxIndex = -1;
@@ -15,23 +19,6 @@ var getMaxTime = function (times) {
   indexMaxTime.push(maxIndex);
   return indexMaxTime;
 };
-var sortByGreater = function (times, names) {
-  var result = -1;
-  var timesAndNames = [];
-  for (var j = 0; j < times.length; j++) {
-    if (times[j] > times[j + 1]) {
-      result = times[j];
-      times[j] = times[j + 1];
-      times[j + 1] = result;
-      result = names[j];
-      names[j] = names[j + 1];
-      names[j + 1] = result;
-    }
-  }
-  timesAndNames.push(times);
-  timesAndNames.push(names);
-  return timesAndNames;
-};
 var getRandomColor = function () {
   var charsAndNumbers = '0123456789ABCDEF';
   var color = '#0000';
@@ -40,51 +27,46 @@ var getRandomColor = function () {
   }
   return color;
 };
-var renderStatisticsAndBadResult = function (ctx, times, names) {
-  ctx.fillStyle = 'white';
-  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
-  ctx.fillRect(100, 10, 420, 270);
-  ctx.shadowColor = 'black';
-  ctx.shadowOffsetX = 0;
-  ctx.shadowOffsetY = 0;
+var renderHistogram = function (ctx, names, badTime, times) {
   ctx.fillStyle = 'black';
-  ctx.font = '16px serif';
+  ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 110, 50);
-  ctx.fillText(' Худшее время: ' + getMaxTime(times)[0] + ' мс у игрока ' + names[getMaxTime(times)[1]], 110, 70);
-};
-window.renderStatistics = function (ctx, names, times) {
-  renderStatisticsAndBadResult(ctx, times, names);
+  ctx.fillText(' Худшее время: ' + badTime[0] + ' мс у игрока ' + names[badTime[1]], 110, 70);
   var positionX = 150;
   var histogramWidth = 40;
   var histogramHeight = 150;
-  var getHistogramHeights = function () {
-    var histogramHeights = [];
-    var maxTime = getMaxTime(times)[0];
-    for (var j = 0; j < times.length; j++) {
-      histogramHeights.push(Math.floor((histogramHeight * times[j]) / maxTime));
-    }
-    return histogramHeights;
-  };
-  var values = sortByGreater(times, names);
+  var histogramHeights = [];
+  var maxTime = getMaxTime(times)[0];
+  for (var j = 0; j < times.length; j++) {
+    histogramHeights.push(Math.floor((histogramHeight * times[j]) / maxTime));
+  }
   for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = 'black';
     ctx.font = '16px PT Mono';
-    ctx.fillText(parseInt(values[0][i], 10), positionX, 245 - getHistogramHeights(getMaxTime(times))[i]);
+    ctx.fillText(parseInt(times[i], 10), positionX, 245 - histogramHeights[i]);
     ctx.fillStyle = 'white';
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = getRandomColor();
     }
-    ctx.fillRect(positionX, 250 - getHistogramHeights(histogramHeight, getMaxTime(times))[i], histogramWidth, getHistogramHeights(getMaxTime(times))[i]);
+    ctx.fillRect(positionX, 250 - histogramHeights[i], histogramWidth, histogramHeights[i]);
     ctx.fillStyle = 'black';
     ctx.font = '16px PT Mono';
-    ctx.fillText(values[1][i], positionX, 270);
+    ctx.fillText(names[i], positionX, 270);
     if (i !== names.length - 1) {
       positionX += 90;
     }
   }
-
+};
+window.renderStatistics = function (ctx, names, times) {
+  var colorCloud = 'rgba(0, 0, 0, 0.7)';
+  var positionXWindow = 110;
+  var positionY = 20;
+  renderCloud(ctx, colorCloud, positionXWindow, positionY);
+  colorCloud = 'white';
+  positionXWindow = 100;
+  positionY = 10;
+  renderCloud(ctx, colorCloud, positionXWindow, positionY);
+  renderHistogram(ctx, names, getMaxTime(times), times);
 };
