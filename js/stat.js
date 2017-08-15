2e8893ab50ba6e953acee1bd6c4a48299c40ee08
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-15 11:09:56 +0300

    Добавлена функция отрисовки гистограмм, удален параметр times из функции renderMaxTime

--- js/stat.js
+++ js/stat.js
@@ -27,35 +27,26 @@ var getRandomColor = function () {
   }
   return color;
 };
-var renderMaxTime = function (ctx, names, badTime, times) {
+var renderMaxTime = function (ctx, names, badTime) {
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 110, 50);
   ctx.fillText(' Худшее время: ' + badTime[0] + ' мс у игрока ' + names[badTime[1]], 110, 70);
 };
-var getHistogramResult = function (ctx, names, times, maxTime) {
-  var positionX = 150;
+var renderHistograms = function (ctx, badTime, names, times, positionX) {
   var histogramWidth = 40;
   var histogramHeight = 150;
-  for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = 'black';
-    ctx.font = '16px PT Mono';
-    ctx.fillText(Math.floor(times[i], 10), positionX, 245 - histogramHeight * Math.floor(times[i]) / maxTime);
-    ctx.fillStyle = 'white';
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = getRandomColor();
-    }
-    ctx.fillRect(positionX, 250 - histogramHeight * Math.floor(times[i]) / maxTime, histogramWidth, histogramHeight * Math.floor(times[i]) / maxTime);
-    ctx.fillStyle = 'black';
-    ctx.font = '16px PT Mono';
-    ctx.fillText(names[i], positionX, 270);
-    if (i !== names.length - 1) {
-      positionX += 90;
-    }
-  }
+  ctx.fillStyle = 'black';
+  ctx.font = '16px PT Mono';
+  ctx.fillText(Math.floor(times, 10), positionX, 245 - histogramHeight * Math.floor(times) / badTime[0]);
+  ctx.fillStyle = 'white';
+  ctx.fillStyle = names === 'Вы' ? 'rgba(255, 0, 0, 1)' : getRandomColor();
+  ctx.fillRect(positionX, 250 - histogramHeight * Math.floor(times) / badTime[0], histogramWidth, histogramHeight * Math.floor(times) / badTime[0]);
+  ctx.fillStyle = 'black';
+  ctx.font = '16px PT Mono';
+  ctx.fillText(names, positionX, 270);
 };
+
 window.renderStatistics = function (ctx, names, times) {
   var colorCloud = 'rgba(0, 0, 0, 0.7)';
   var positionXWindow = 110;
@@ -66,5 +57,9 @@ window.renderStatistics = function (ctx, names, times) {
   positionY = 10;
   renderCloud(ctx, colorCloud, positionXWindow, positionY);
   renderMaxTime(ctx, names, getMaxTime(times), times);
-  getHistogramResult(ctx, names, times, getMaxTime(times)[0]);
+  var positionX = 150;
+  for (var i = 0; i < names.length; i++) {
+    renderHistograms(ctx, getMaxTime(times), names[i], times[i], positionX);
+    positionX += 90;
+  }
 };
