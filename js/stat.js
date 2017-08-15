a336d0bcceee9090332494bc97695506a34bef10
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-15 12:42:51 +0300

    Добавлены переменные с длинами массивов, исправлено количество параметров принимаемых функцией renderMaxTime, исправлена функция getRandomColor

--- js/stat.js
+++ js/stat.js
@@ -4,11 +4,12 @@ var renderCloud = function (ctx, colorCloud, x, y) {
   ctx.fillStyle = colorCloud;
   ctx.fillRect(x, y, 420, 270);
 };
-var getMaxTime = function (times) {
+
+var getMaxTime = function (times, timesLength) {
   var max = -1;
   var maxIndex = -1;
   var indexMaxTime = [];
-  for (var i = 0; i < times.length; i++) {
+  for (var i = 0; i < timesLength; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
@@ -19,47 +20,49 @@ var getMaxTime = function (times) {
   indexMaxTime.push(maxIndex);
   return indexMaxTime;
 };
+
 var getRandomColor = function () {
-  var charsAndNumbers = '0123456789ABCDEF';
-  var color = '#0000';
-  for (var i = 0; i < 2; i++) {
-    color += charsAndNumbers[Math.floor(Math.random() * 16)];
-  }
+
+  var color = 'rgba(11, 19, 181, ';
+  color += Math.random().toFixed(1) + ')';
   return color;
 };
+
 var renderMaxTime = function (ctx, names, badTime) {
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 110, 50);
   ctx.fillText(' Худшее время: ' + badTime[0] + ' мс у игрока ' + names[badTime[1]], 110, 70);
 };
+
 var renderHistograms = function (ctx, badTime, names, times, positionX) {
   var histogramWidth = 40;
   var histogramHeight = 150;
+  var HeightCalculation = histogramHeight * Math.floor(times) / badTime[0];
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
-  ctx.fillText(Math.floor(times, 10), positionX, 245 - histogramHeight * Math.floor(times) / badTime[0]);
-  ctx.fillStyle = 'white';
+  ctx.fillText(Math.floor(times), positionX, 245 - HeightCalculation);
   ctx.fillStyle = names === 'Вы' ? 'rgba(255, 0, 0, 1)' : getRandomColor();
-  ctx.fillRect(positionX, 250 - histogramHeight * Math.floor(times) / badTime[0], histogramWidth, histogramHeight * Math.floor(times) / badTime[0]);
+  ctx.fillRect(positionX, 250 - HeightCalculation, histogramWidth, HeightCalculation);
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
   ctx.fillText(names, positionX, 270);
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  var colorCloud = 'rgba(0, 0, 0, 0.7)';
   var positionXWindow = 110;
   var positionY = 20;
-  renderCloud(ctx, colorCloud, positionXWindow, positionY);
-  colorCloud = 'white';
+  var namesLength = names.length;
+  var timesLength = times.length;
+  renderCloud(ctx, 'rgba(0, 0, 0, 0.7)', positionXWindow, positionY);
   positionXWindow = 100;
   positionY = 10;
-  renderCloud(ctx, colorCloud, positionXWindow, positionY);
-  renderMaxTime(ctx, names, getMaxTime(times), times);
+  var badTime = getMaxTime(times, timesLength);
+  renderCloud(ctx, 'white', positionXWindow, positionY);
+  renderMaxTime(ctx, names, badTime);
   var positionX = 150;
-  for (var i = 0; i < names.length; i++) {
-    renderHistograms(ctx, getMaxTime(times), names[i], times[i], positionX);
+  for (var i = 0; i < namesLength; i++) {
+    renderHistograms(ctx, badTime, names[i], times[i], positionX);
     positionX += 90;
   }
 };
