23a5126b32cfec27ddea410282f9175cadea45fb
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-15 17:21:06 +0300

    Удалены строки 40,41,46(лишнее переназанчение шрифта и цвета), переменная heightCalculation удалена, формула расчета высоты сохранена в переменной histogramHeight

--- js/stat.js
+++ js/stat.js
@@ -33,17 +33,14 @@ var renderMaxTime = function (ctx, names, badTime) {
   ctx.fillText(' Худшее время: ' + badTime[0] + ' мс у игрока ' + names[badTime[1]], 110, 70);
 };
 
-var renderHistograms = function (ctx, badTime, names, times, positionX) {
+var renderHistograms = function (ctx, MaxTime, names, times, positionX) {
   var histogramWidth = 40;
   var histogramHeight = 150;
-  var heightCalculation = histogramHeight * Math.floor(times) / badTime[0];
-  ctx.fillStyle = 'black';
-  ctx.font = '16px PT Mono';
-  ctx.fillText(Math.floor(times), positionX, 245 - heightCalculation);
+  histogramHeight *= Math.floor(times) / MaxTime[0];
+  ctx.fillText(Math.floor(times), positionX, 245 - histogramHeight);
   ctx.fillStyle = names === 'Вы' ? 'rgba(255, 0, 0, 1)' : getRandomColor();
-  ctx.fillRect(positionX, 250 - heightCalculation, histogramWidth, heightCalculation);
+  ctx.fillRect(positionX, 250 - histogramHeight, histogramWidth, histogramHeight);
   ctx.fillStyle = 'black';
-  ctx.font = '16px PT Mono';
   ctx.fillText(names, positionX, 270);
 };
 
@@ -54,12 +51,12 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 'rgba(0, 0, 0, 0.7)', positionXWindow, positionY);
   positionXWindow = 100;
   positionY = 10;
-  var badTime = getMaxTime(times);
+  var MaxTime = getMaxTime(times);
   renderCloud(ctx, 'white', positionXWindow, positionY);
-  renderMaxTime(ctx, names, badTime);
+  renderMaxTime(ctx, names, MaxTime);
   var positionX = 150;
   for (var i = 0; i < namesLength; i++) {
-    renderHistograms(ctx, badTime, names[i], times[i], positionX);
+    renderHistograms(ctx, MaxTime, names[i], times[i], positionX);
     positionX += 90;
   }
 };
