2631be881cac58cc5dfab1ac22956a8a929128fe
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-16 20:58:48 +0300

    Добавлена функция generateNames, и 2 массива(имена, фамилии)

--- js/setup.js
+++ js/setup.js
@@ -1,17 +1,30 @@
 'use strict';
 
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+
+var WIZARD_lAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+
+
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
-
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-var WIZARD_NAMES = ['Волшебник_1', 'Волшебник_2', 'Волшебник_3', 'Волшебник_4'];
+var namesLenght = WIZARD_NAMES.length;
 
-for (var i = 0; i < WIZARD_NAMES; i++) {
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+var generateNames = function (max) {
+  return Math.floor(Math.random() * (max - 1));
+};
+
+for (var i = 0; i < 4; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = WIZARD_NAMES[i];
+  wizardElement.querySelector('.setup-similar-label').textContent =  WIZARD_NAMES[generateNames(namesLenght)] +
+    ' ' + WIZARD_lAST_NAMES[generateNames(namesLenght)];
+  console.log(generateNames(namesLenght));
   similarListElement.appendChild(wizardElement);
 }
+
+
