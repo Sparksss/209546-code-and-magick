59866dc1b36c37acaa683ccb6696c481512ce2b1
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-17 20:08:10 +0300

    Переименовал функцию getRandomWizard в getRandomeNumber, добавил переменные randomNumber в которой содержиться случайное число, добавил переменную wizard, в которой содержиться объект с параметрами для рендеринга волшебников

--- js/setup.js
+++ js/setup.js
@@ -6,18 +6,19 @@ userDialog.classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-var generateNames = function () {
-  return Math.round(0 + Math.random() * (8 + 1 - 0));
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)', 'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'];
+
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var generateNumbers = function () {
+  return Math.round(Math.random() * 8);
 };
 
 var getRandomWizards = function (number) {
-  var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)', 'rgb(215, 210, 55)',
-    'rgb(0, 0, 0)'];
-
-  var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
   return {name: WIZARD_NAMES[number] + ' ' + WIZARD_LAST_NAMES[number], coat: coatColor[number], eyes: eyesColor[number]};
 };
 
@@ -25,9 +26,11 @@ var renderWizards = function (template) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < 4; i++) {
     var wizardElement = template.cloneNode(true);
-    wizardElement.querySelector('.setup-similar-label').textContent = getRandomWizards(generateNames())['name'];
-    wizardElement.querySelector('.wizard-coat').style.fill = getRandomWizards(generateNames())['coat'];
-    wizardElement.querySelector('.wizard-eyes').style.fill = getRandomWizards(generateNames())['eyes'];
+    var randomNumber = generateNumbers();
+    var wizard = getRandomWizards(randomNumber, WIZARD_NAMES, WIZARD_LAST_NAMES, coatColor, eyesColor);
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard['name'];
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard['coat'];
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard['eyes'];
     fragment.appendChild(wizardElement);
   }
   return fragment;
