2f928764d13fe239fbf804841483c1e455c80937
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-17 20:15:39 +0300

    Записал массив в столбик

--- js/setup.js
+++ js/setup.js
@@ -8,9 +8,14 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template').c
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)', 'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'];
+var coatColor = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
 
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 
@@ -35,7 +40,6 @@ var renderWizards = function (template) {
   }
   return fragment;
 };
-
 
 similarListElement.appendChild(renderWizards(similarWizardTemplate));
 
