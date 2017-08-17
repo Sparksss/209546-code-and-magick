f756c1b6b83bc312bd8abc0ec871aac126700f21
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-17 12:28:18 +0300

    Добавлены случайные цвета для плащей и глаз волшебника

--- js/setup.js
+++ js/setup.js
@@ -1,28 +1,32 @@
 'use strict';
 
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-
-var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-
-
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-var namesLenght = WIZARD_NAMES.length;
-
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
-var generateNames = function (max) {
-  return Math.floor(Math.random() * (max - 1));
+var generateNames = function () {
+  return Math.round(1 + Math.random() * (9 + 1 - 1));
+};
+
+var getRandomWizards = function (number) {
+  var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)', 'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'];
+
+  var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+  return {name: WIZARD_NAMES[number] + ' ' + WIZARD_LAST_NAMES[number], coat: coatColor[number], eyes: eyesColor[number]};
 };
 
 for (var i = 0; i < 4; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = WIZARD_NAMES[generateNames(namesLenght)] + ' ' + WIZARD_LAST_NAMES[generateNames(namesLenght)];
+  wizardElement.querySelector('.setup-similar-label').textContent = getRandomWizards(generateNames())['name'];
+  wizardElement.querySelector('.wizard-coat').style.fill = getRandomWizards(generateNames())['coat'];
+  wizardElement.querySelector('.wizard-eyes').style.fill = getRandomWizards(generateNames())['eyes'];
   similarListElement.appendChild(wizardElement);
 }
-
-
