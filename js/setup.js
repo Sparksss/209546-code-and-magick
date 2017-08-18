9c9582a8705664c5fa6efec7aac76dd3d9ef3766
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-18 11:39:59 +0300

    Исправлена функция renderWizards, цикл вынесен из функции

--- js/setup.js
+++ js/setup.js
@@ -19,28 +19,31 @@ var coatColor = [
 
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var fragment = document.createDocumentFragment();
+
 var getRandomNumber = function (max) {
   return Math.floor(Math.random() * max);
 };
 
-var getRandomWizards = function (randomNumber) {
-  var number = randomNumber(WIZARD_NAMES.length);
-  return {name: WIZARD_NAMES[number] + ' ' + WIZARD_LAST_NAMES[number], coat: coatColor[randomNumber(coatColor.length)], eyes: eyesColor[randomNumber(eyesColor.length)]};
+var getRandomWizards = function () {
+  var number = getRandomNumber(WIZARD_NAMES.length);
+  return {name: WIZARD_NAMES[number] + ' ' + WIZARD_LAST_NAMES[number], coat: coatColor[getRandomNumber(coatColor.length)], eyes: getRandomNumber()[getRandomNumber(eyesColor.length)]};
 };
 
 var renderWizards = function (template) {
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < 4; i++) {
-    var wizardElement = template.cloneNode(true);
-    var wizard = getRandomWizards(getRandomNumber);
-    wizardElement.querySelector('.setup-similar-label').textContent = wizard['name'];
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard['coat'];
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard['eyes'];
-    fragment.appendChild(wizardElement);
-  }
-  return fragment;
+  var wizardElement = template.cloneNode(true);
+  var wizard = getRandomWizards();
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard['name'];
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard['coat'];
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard['eyes'];
+  return wizardElement;
 };
 
-similarListElement.appendChild(renderWizards(similarWizardTemplate));
+for (var i = 0; i < 4; i++) {
+  fragment.appendChild(renderWizards(similarWizardTemplate));
+}
+
+similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
